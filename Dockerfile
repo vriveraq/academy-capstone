3f5eef22ed0cb812255178e094f92e3a22621a3d
Vanessa Rivera-Quinones <vriveraq.phd@gmail.com>
2023-09-14 14:52:14 +0000

    Afternoon work, to fix end point

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,7 @@
 FROM public.ecr.aws/dataminded/spark-k8s-glue:v3.1.2-hadoop-3.3.1
-
+USER root
+WORKDIR /repo
+COPY . /repo
+RUN  pip install --upgrade pip && pip install -r requirements.txt
+ENTRYPOINT ["python3"]
+CMD ["ETL_weather_pipe.py"]
--- ETL_weather_pipe.py
+++ ETL_weather_pipe.py
@@ -4,8 +4,8 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import isnan, when, count, col
 
 # Save data from S3 bucket 
-s3_path = f"s3a://dataminded-academy-capstone-resources/raw/open_aq/"
-config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.2.0", "fs.s3a.aws.credentials.provider": "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"}
+s3_path = "s3a://dataminded-academy-capstone-resources/raw/open_aq/"
+config = {"spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.1.2", "fs.s3a.aws.credentials.provider": "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"}
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf = conf).getOrCreate()
 df = spark.read.json(s3_path)
